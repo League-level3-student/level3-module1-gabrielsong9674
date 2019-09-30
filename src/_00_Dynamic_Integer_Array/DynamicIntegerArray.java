a3a1819f5a1161ab70f44a6c186f7411ef45c163
gabrielsong9674 <gracegabrielson@icloud.com>
2019-09-29 17:23:45 -0700

    guestBook(IP)

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -16,14 +16,15 @@ public class DynamicIntegerArray {
 		int[] longer = new int[num.length+1];
 		//B. set the last element of your new array to 
 		//   the value passed into the method
-		longer[num.length+1] = v;
+		longer[longer.length-1] = v;
 		//C. iterate through the member array and 
 		//   copy every element from the member array 
 		//   to the new array
 		for (int i = 0; i < num.length; i++) {
-			
+			longer[i] = num[i];
 		}
 		//D. set the member array equal to the new array.
+		num = longer;
 	}
 	
 	//4. Complete the steps in the get method
@@ -39,26 +40,37 @@ public class DynamicIntegerArray {
 	public void set(int v, int location) {
 		//A. set the variable at the location passed in to the method
 		//   to the new value v
+		num[location] = v;
 	}
 	
 	//7. Complete the steps in the insert method
 	public void insert(int v, int location) {
 		//A. create and initialize a new int array to be one 
 		//   element longer than the member array
-		
+		int[] longer = new int[num.length + 1];
 		//B. Make a for loop that iterates through the new array
-
+		for (int i = 0; i < longer.length; i++) 
+		{
 			//C. if i is less than location:
 		    //		set the element at i of the new array to the element at i of the member array
-			
+			if(i < location)
+			{
+				longer[i] = num[i];
+			}
 			//D. else if i is greater than location:
 		    //		set the element at i of the new array to the i - 1 element of the member array
-			
+			else if(i > location)
+			{
+				longer[i] = num[i-1];
+			}
 			//E. else, set the element at i of the new array to the value v
-		
-		
+			else
+			{
+				longer[i] = v;
+			}
+		}
 		//F. set the member array equal to the new array
-	
+		num = longer;
 	}
 	
 	//8. Run the tests again and check your progress
@@ -66,30 +78,44 @@ public class DynamicIntegerArray {
 	//9. Complete the steps in the remove method
 	public void remove(int location) {
 		//A. create a new array that is one element smaller than the member array
-		
+		int[] smaller = new int[num.length -1];
 		//B. make a for loop to iterate through the member array
-		
+		for (int i = 0; i < smaller.length; i++)
+		{
 			//C. if i  is less than location
 			//		set the element at i of the new array to the element at i of the member array
-			
+			if(i < location)
+			{
+				smaller[i] = num[i];
+			}
 			//D. else if i  is greater than location
 			//		set the element at i - 1 of the new array to the element at i of the member array
-			
+			else if(i > location)
+			{
+				smaller[i-1] = num[i];
+			}
 			//E. else, continue;
-			
+			else
+			{	
+				smaller[i]=num[i+1];
+			}
+		}
 		//F. set the member array equal to the new array
+		num = smaller;
 	}
 	
 	//10. Run the tests again the see if you are correct so far
 	
 	//11. Complete the size method so that it returns the length of the member array.
 	public int size() {
-		return 0;
+		return num.length;
 	}
 	
 	//12. Complete the clear array so that it sets the member array 
 	//    equal to a new integer array of size 0
 	public void clear() {
+		int[] nothing = new int[0];
+		num = nothing;
 	}
 	
 	//13. Run the test again to see if you are finished.
--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,6 +1,15 @@
 package _01_IntroToArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,7 +18,42 @@ public class _02_GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	public static void main(String[] args) {
+		_02_GuestBook book = new _02_GuestBook();
+		book.makeGUI();
+	}
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton("Add Name");
+	JButton view = new JButton("View Name");
 	
-	
+	public void makeGUI()
+	{
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setSize(500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel.add(add);
+		add.addActionListener(this);
+		panel.add(view);
+		view.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) e.getSource();
+		ArrayList<String> names = new ArrayList<String>(4);
+		if(buttonPressed == add)
+		{
+			String userInput = JOptionPane.showInputDialog("Enter a name");
+			names.add(userInput);
+			
+		}
+		if(buttonPressed == view)
+		{
+			System.out.println(names.get(0));
+		}
+	}
 	
 }
